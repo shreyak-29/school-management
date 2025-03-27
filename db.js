7b14e5e0cb798c1011f55eeeee4e18a436055f76
Shreya Kathe <shreyakathe2904@gmail.com>
2025-03-27 21:57:22 +0530

    hosted mySql

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
+// Connect to the database
 connection.connect(err => {
   if (err) {
-    console.error('Error connecting to MySQL: ', err);
+    console.error('Error connecting to MySQL:', err);
     process.exit(1);
   }
   console.log('Connected to MySQL database.');
