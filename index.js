87b0df12dc6331e41fecb4455a025b41f2d7dec9
Shreya Kathe <shreyakathe2904@gmail.com>
2025-08-06 22:13:36 +0530

    .

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use((req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
 });
+
